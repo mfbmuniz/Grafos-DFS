20ef267bee9317e8633eb3086742973e03ab0f45
Marcos Felipe Batista Muniz <filips8@hotmail.com>
2021-05-05 15:26:51 -0300

    tp faltando ajustar caminhamento pr vertice 5

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -155,16 +155,19 @@ public class Main {
     }
 
     public static List detectCycles (Vertex[] grafo){
-        List ciclosSomados = new ArrayList<Vertex>();
-        for (Vertex u : grafo) {
+        List ciclosSomados = new ArrayList<List<Vertex>>();
+        /*for (Vertex u : grafo) {
+            //ciclosSomados.addAll( dfs(grafo,u) );
+            List<List> listaTemp = new ArrayList<>();
+            listaTemp.addAll( dfs(grafo,u) );
 
+            for(List<Vertex> l :  listaTemp){
+                inserirNaLista(l,ciclosSomados);
+            }
 
-            ciclosSomados.addAll( dfs(grafo,u) );
-            int auxdebug1 = 0 ;
-
-        }
+        }*/
+        ciclosSomados.addAll( dfs(grafo,(Vertex) grafo[5]) );
         int auxdebug2 =0;
-        retirarRepetidos(ciclosSomados);
         print(ciclosSomados);
         System.out.println("Total de ciclos: " +ciclosSomados.size());
         return ciclosSomados;
@@ -201,7 +204,6 @@ public class Main {
             }
 
             System.out.println(" "+verticeInicial.getNumVertice());
-            retirarRepetidos(ciclos);
             int h=0;
             print(ciclos);
             return ciclos;
@@ -278,17 +280,38 @@ public class Main {
                 cicloEncontradoTratado = cicloEncontrado.subList(indiceVizinhoAtual, indexVerticeAtual+1);
                 cicloEncontradoTratado = ordenaInsercao(cicloEncontradoTratado);
                 cicloEncontradoTratado.add(cicloEncontradoTratado.get(0)); //era o vizinho
-                ciclos.add(cicloEncontradoTratado);
+                inserirNaLista(cicloEncontradoTratado,ciclos);   //ciclos.add(cicloEncontradoTratado);
             }else if (vizinhoAtual.getCor() == 2) {
                 cicloEncontradoTratado = cicloEncontrado.subList(indexVerticeAtual,indiceVizinhoAtual+1);
                 cicloEncontradoTratado = ordenaInsercao(cicloEncontradoTratado);
-                cicloEncontradoTratado.add(cicloEncontradoTratado.get(0));
-                ciclos.add(cicloEncontradoTratado);
+                cicloEncontradoTratado.add(cicloEncontradoTratado.get(0)); //era o atual
+                inserirNaLista(cicloEncontradoTratado,ciclos);
             }
         }catch (Exception e){
             e.printStackTrace();
             System.exit(0);
         }
+
+    }
+
+    private static void inserirNaLista(List<Vertex> cicloEncontradoTratado, List<List<Vertex>> ciclos) {
+        String ciclo_atual;
+        String ciclo_insercao="";
+        boolean flag=false;
+        for (Vertex v: cicloEncontradoTratado){
+            ciclo_insercao+=""+v.getNumVertice();
+        }
+        for (List<Vertex> vertexList: ciclos) {
+            ciclo_atual="";
+            for( Vertex v : vertexList  ){
+                ciclo_atual+=""+v.getNumVertice();
+            }
+            if (ciclo_atual.equals(ciclo_insercao)){
+                flag = true;
+                return;
+            }
+        }
+        ciclos.add(cicloEncontradoTratado);
 
     }
 
@@ -302,13 +325,31 @@ public class Main {
             }
         }
         List cicloOrdenado = new ArrayList<Vertex>();
-        int j = 0;
-        for (int i= valor_posicao_DoMenorElemento[1]  ; j< cicloEncontradoTratado.size();j++){
 
-            cicloOrdenado.add(cicloEncontradoTratado.get(i++));
+        int posAnterior = valor_posicao_DoMenorElemento[1]-1 < 0 ? cicloEncontradoTratado.size()-1 : valor_posicao_DoMenorElemento[1]-1;
+        int posPosterior = valor_posicao_DoMenorElemento[1]+1 > cicloEncontradoTratado.size()-1 ?0:valor_posicao_DoMenorElemento[1]+1;
 
-            if (i == cicloEncontradoTratado.size()){
-                i= 0;
+        boolean flagFoward = cicloEncontradoTratado.get(posAnterior).getNumVertice()  > cicloEncontradoTratado.get(posPosterior).getNumVertice()?true:false;
+
+        if(flagFoward) { //caminha pra frente
+            int j = 0;
+            for (int i = valor_posicao_DoMenorElemento[1]; j < cicloEncontradoTratado.size(); j++) {
+
+                cicloOrdenado.add(cicloEncontradoTratado.get(i++));
+
+                if (i == cicloEncontradoTratado.size()) {
+                    i = 0;
+                }
+            }
+        }else{ //caminha pra traz
+            int j = 0;
+            for (int i = valor_posicao_DoMenorElemento[1]; j < cicloEncontradoTratado.size(); j++) {
+
+                cicloOrdenado.add(cicloEncontradoTratado.get(i--));
+
+                if (i < 0  ) {
+                    i = cicloEncontradoTratado.size()-1; //ou tamanho -1
+                }
             }
         }
         return (ArrayList<Vertex>) cicloOrdenado;
@@ -322,9 +363,9 @@ public class Main {
             cicloEncontrado.add(verticeAtual.clone());
             cicloEncontrado.add(vizinhoAtual.clone());
             cicloEncontrado.add(vizinhoDoVizinho.clone());
-            cicloEncontrado.add(verticeAtual.clone());
             cicloEncontrado = ordenaInsercao(cicloEncontrado);
-            ciclos.add(cicloEncontrado);
+            cicloEncontrado.add(cicloEncontrado.get(0));
+            inserirNaLista(cicloEncontrado,ciclos);
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -358,7 +399,7 @@ public class Main {
                             List<Vertex> temp_CicloRetiradoDeCiclos = (List<Vertex>) ciclos.get(j);
                             if (temp_CicloRetiradoDeCiclos.size() != cicloAtual.size()) {
                                 continue; //doing nothing.
-                            } else {
+                            } else{
                                 for (Vertex v : temp_CicloRetiradoDeCiclos) {
                                     if (!contemElemento(v, cicloAtual)) {
                                         flag = false;
